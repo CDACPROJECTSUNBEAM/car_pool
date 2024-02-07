acc747178a8b6f9d793f7106351058d225bc161b
Akshat-Shah-22 <akshatshah535@gmail.com>
2024-02-07 17:20:34 +0530

    Task - 7 ( Get all vehicles )

--- spring_boot_backend_security_jwt_template/src/main/java/com/app/controllers/AdminController.java
+++ spring_boot_backend_security_jwt_template/src/main/java/com/app/controllers/AdminController.java
@@ -1,5 +1,25 @@
 package com.app.controllers;
 
+import com.app.dtos.VehicleDTO;
+import com.app.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
 public class AdminController {
 
+    @Autowired
+    private VehicleService vService;
+
+    @GetMapping("/vehicles")
+    public ResponseEntity<List<VehicleDTO>> getAllVehicles(){
+        return new ResponseEntity<List<VehicleDTO>>(vService.getAllVehicles(), HttpStatus.OK);
+    }
 }
