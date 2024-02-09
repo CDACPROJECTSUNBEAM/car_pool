bad7926fb80c376a3fb17fe97add0539cdb6f709
Akshat-Shah-22 <akshatshah535@gmail.com>
2024-02-09 15:28:50 +0530

    Task - 13 ( Admin Controller )

--- spring_boot_backend_security_jwt_template/src/main/java/com/app/controllers/AdminController.java
+++ spring_boot_backend_security_jwt_template/src/main/java/com/app/controllers/AdminController.java
@@ -1,13 +1,15 @@
 package com.app.controllers;
 
+import com.app.dtos.CityDTO;
+import com.app.dtos.StateDTO;
 import com.app.dtos.VehicleDTO;
+import com.app.services.AdminService;
+import com.app.services.RegisterService;
 import com.app.services.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +20,55 @@ public class AdminController {
     @Autowired
     private VehicleService vService;
 
+    @Autowired
+    private RegisterService rService;
+
+    @Autowired
+    private AdminService aService;
+
+
+
     @GetMapping("/vehicles")
     public ResponseEntity<List<VehicleDTO>> getAllVehicles(){
         return new ResponseEntity<List<VehicleDTO>>(vService.getAllVehicles(), HttpStatus.OK);
     }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getAllUsers(){
+        return new ResponseEntity<>(aService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/drivers")
+    public ResponseEntity<?> getAllDrivers(){
+        return new ResponseEntity<>(aService.getAllDrivers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/bookings")
+    public ResponseEntity<?> getAllBookings(){
+        return new ResponseEntity<>(aService.getAllBookings(), HttpStatus.OK);
+
+    }
+    @GetMapping("/rides")
+    public ResponseEntity<?> getAllRides(){
+        return new ResponseEntity<>(aService.getAllRides(), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/cities")
+    public ResponseEntity<?> getAllCities(){
+        return new ResponseEntity<>(aService.getAllCities(), HttpStatus.OK);
+
+    }
+
+    @PostMapping("/add/state")
+    public ResponseEntity<?> addState(@RequestBody StateDTO sdto){
+        return new ResponseEntity<>(aService.addState(sdto), HttpStatus.OK);
+    }
+
+    @PostMapping("/add/city")
+    public ResponseEntity<?> addCity(@RequestBody CityDTO cdto){
+        return new ResponseEntity<>(aService.addCity(cdto), HttpStatus.OK);
+    }
+
+
 }
